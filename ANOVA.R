25bcfd96edcd51815763c2f141924061c74e835e
Anastasiia <138507200+serputkona@users.noreply.github.com>
2024-08-15 15:16:20 -0400

    Update ANOVA.R

--- ANOVA.R
+++ ANOVA.R
@@ -1,9 +1,31 @@
-setwd('D:/R_directory')
-b<-read.table(file='Ex.csv',header=TRUE,sep=',')
-View(b)
-names(b)
+getwd()
+b  <- read.csv("Ex.csv")
+b
+l <- b$Level.of.relaxation
+t <- b$Types
 library(nortest)
+library(ggplot2) 
 
-boxplot(Level.of.relaxation ~ Types, data = b,
-        xlab = "Типи валеріани",  ylab ="Рівень розслабленості", col =
-          c('pink',"red",'yellow','green'))
+stripchart(l ~ t, xlab = "Level of relaxation",  
+           ylab ="Types", col=c("blueviolet", "cyan4", "brown2", "green3"), 
+           method = "jitter")
+
+boxplot(l ~ t, data = b,
+        xlab = "Level of relaxation",  
+        ylab ="Types", col=c("blueviolet", "cyan4", "brown2", "green3"))
+
+aggregate(x = l, by = list(t), FUN = mean)
+
+mod1 <- aov(l ~ t, data = b)
+summary(aov(l ~ t, data = b))
+
+summary(lm(l ~ t, data = b))
+
+bartlett.test(l ~ t)
+
+tapply(l, t, var)
+
+library(ggpubr)
+ggqqplot(mod1$residuals, color = "mediumpurple1")
+
+shapiro.test(mod1$residuals)
